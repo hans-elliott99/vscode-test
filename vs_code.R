18ca07a216a365265eb683a5f09e46729aec9656
Hans Elliott <hanselliott61@gmail.com>
2022-04-04 12:25:52 -0700

    add R script

--- vs_code.R
+++ vs_code.R
@@ -1,5 +1,7 @@
 ## R Script Written in VS Code
-library("dplyr", "ggplot2")
+library(dplyr, ggplot2)
 
 df = mtcars
 df %>% select(mpg, cyl) %>% head()
+
+df %>% ggplot2::ggplot(ggplot2::aes(x = mpg, y = cyl)) + ggplot2::geom_point()
